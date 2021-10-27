bbcfb1be1dae0c2fc94b76b98e757550316bbcee
Sutjapong <sutjapong@conceptreach.com>
2021-10-27 21:21:46 +0700

    refactor: Try remove perge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,15 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: [
-    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    './vendor/laravel/jetstream/**/*.blade.php',
-    './storage/framework/views/*.php',
-    './resources/views/**/*.blade.php',
-    './resources/js/**/*.vue',
-  ],
+  purge: {
+    enabled: false,
+    content: [
+      './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+      './vendor/laravel/jetstream/**/*.blade.php',
+      './storage/framework/views/*.php',
+      './resources/views/**/*.blade.php',
+      './resources/js/**/*.vue',],
+  },
 
   theme: {
     extend: {
