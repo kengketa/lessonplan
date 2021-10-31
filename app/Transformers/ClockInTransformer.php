13e30013130f1d1f8c2048d556ae0f9d183f1866
Sutjapong <sutjapong@conceptreach.com>
2021-10-31 09:04:54 +0700

    bug: Fix carbon parse null to today

--- app/Transformers/ClockInTransformer.php
+++ app/Transformers/ClockInTransformer.php
@@ -15,8 +15,8 @@ class ClockInTransformer extends TransformerAbstract
             'teacher_id' => $clockIn->teacher_id,
             'school_id' => $clockIn->school_id,
             'date' => Carbon::parse($clockIn->date)->format('d M Y'),
-            'clock_in' => Carbon::parse($clockIn->clock_in)->format('h:i:sa'),
-            'clock_out' => Carbon::parse($clockIn->clock_out)->format('h:i:sa'),
+            'clock_in' => $clockIn->clock_in ? Carbon::parse($clockIn->clock_in)->format('h:i:sa') : null,
+            'clock_out' => $clockIn->clock_out ? Carbon::parse($clockIn->clock_out)->format('h:i:sa') : null,
         ];
 
         return $data;
