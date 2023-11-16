c168afb922f798d51febb17810b151c21239c2ec
Kengketa <kengketa@gmail.com>
2023-11-16 16:59:23 +0700

    feat: add calendar button

--- resources/js/Pages/Dashboard/Schools/Show.vue
+++ resources/js/Pages/Dashboard/Schools/Show.vue
@@ -2,35 +2,35 @@
   <div>
     <ConfirmDialog
       v-model="isShowDeleteDialog"
-      title="Delete Product?"
       :body="`The School: ${ school.name} and its related data will be deleted. This action cannot be undone.`"
-      confirm-text="Delete School"
       :confirm-event="deleteSchool"
+      confirm-text="Delete School"
+      title="Delete Product?"
     />
     <ConfirmDialog
       v-model="showDeleteGradeDialog"
-      title="Delete Grade?"
       :body="`Grade: ${ deletingGrade ? deletingGrade.name : null} will be deleted.`"
-      confirm-text="Delete Grade"
       :confirm-event="deleteGrade"
+      confirm-text="Delete Grade"
+      title="Delete Grade?"
     />
     <ConfirmDialog
       v-model="showDeleteSubjectDialog"
-      title="Delete subject?"
       :body="`Subject: ${ deletingSubject ? deletingSubject.name : null} will be deleted.`"
-      confirm-text="Delete Subject"
       :confirm-event="deleteSubject"
+      confirm-text="Delete Subject"
+      title="Delete subject?"
     />
     <ConfirmDialog
       v-model="showRemoveTeacherDialog"
-      title="Remove teacher from this school?"
       :body="`Teacher: ${ removingTeacher ? removingTeacher.name : null} will be removed from this school.`"
-      confirm-text="Remove"
       :confirm-event="removeTeacher"
+      confirm-text="Remove"
+      title="Remove teacher from this school?"
     />
     <Breadcrumbs
-      :breadcrumbs="breadcrumbs"
       :back="route('dashboard')"
+      :breadcrumbs="breadcrumbs"
     />
     <PageHeading>
     <span class="hidden lg:inline">
@@ -38,17 +38,24 @@
     </span>
       <template #actions>
         <Link
+          :href="route('dashboard.schools.calendar', school.id)"
+          class="button button-primary mr-2"
+        >
+          <CalendarIcon aria-hidden="true" class="h-5 w-5 mr-2"/>
+          Calendar
+        </Link>
+        <Link
           :href="route('dashboard.misbehaviors.index', school.id)"
           class="button button-primary mr-2"
         >
-          <DocumentReportIcon class="h-5 w-5 mr-2" aria-hidden="true" />
+          <DocumentReportIcon aria-hidden="true" class="h-5 w-5 mr-2"/>
           MB Report
         </Link>
         <Link
           :href="route('dashboard.reports.create', school.id)"
           class="button button-primary mr-2"
         >
-          <DocumentReportIcon class="h-5 w-5 mr-2" aria-hidden="true" />
+          <DocumentReportIcon aria-hidden="true" class="h-5 w-5 mr-2"/>
           Create Lesson plan
         </Link>
         <Link
@@ -56,13 +63,13 @@
           :href="route('dashboard.schools.edit', school.id)"
           class="button button-primary mr-2"
         >
-          <PencilIcon class="h-5 w-5 mr-2" aria-hidden="true" />
+          <PencilIcon aria-hidden="true" class="h-5 w-5 mr-2"/>
           Edit
         </Link>
         <form v-if="$page.props.authUserRole === 'ADMIN' || $page.props.authUserRole === 'SUPER_ADMIN'"
               class="inline-flex" @submit.prevent="isShowDeleteDialog = true">
-          <button type="submit" class="button button-danger">
-            <TrashIcon class="h-5 w-5 mr-2" aria-hidden="true" />
+          <button class="button button-danger" type="submit">
+            <TrashIcon aria-hidden="true" class="h-5 w-5 mr-2"/>
             Delete
           </button>
         </form>
@@ -71,49 +78,49 @@
     <section class="mt-4">
       <div class="md:grid md:grid-cols-6 md:gap-2">
         <SearchSelectInput
-          :options="school.years"
-          :is-show-line="false"
           v-model="filterForm.filters.academic_year"
+          :is-show-line="false"
+          :options="school.years"
           label="Academicyear"
         />
         <SearchSelectInput
-          :options="school.semesters"
           v-model="filterForm.filters.semester"
           :is-show-line="false"
+          :options="school.semesters"
           label="Semester"
         />
         <SearchSelectInput
-          :options="school.teachers"
-          :is-show-line="false"
           v-model="filterForm.filters.teacher"
+          :is-show-line="false"
+          :options="school.teachers"
           label="Teacher"
         />
         <SearchSelectInput
-          :options="school.grades.data"
           v-model="filterForm.filters.grade"
           :is-show-line="false"
+          :options="school.grades.data"
           label="filter class"
         />
         <SearchSelectInput
-          :options="school.subjects"
           v-model="filterForm.filters.subject"
           :is-show-line="false"
+          :options="school.subjects"
           label="filter subject"
         />
         <SearchSelectInput
-          :options="school.weeks"
           v-model="filterForm.filters.week"
           :is-show-line="false"
+          :options="school.weeks"
           label="week"
         />
         <div class="flex items-end mt-2 md:mt-0">
-          <button @click="print()" type="button" class="button button-primary button-small mb-1">Print</button>
-          <button @click="clearFilter()" type="button" class="button button-primary button-small mb-1 ml-1">
+          <button class="button button-primary button-small mb-1" type="button" @click="print()">Print</button>
+          <button class="button button-primary button-small mb-1 ml-1" type="button" @click="clearFilter()">
             Clear
           </button>
           <button v-if="$page.props.authUserRole === 'ADMIN' || $page.props.authUserRole === 'SUPER_ADMIN'"
-                  @click="showSuperApproveModal = true" type="button"
-                  class="button button-primary button-small mb-1 ml-1 whitespace-nowrap">
+                  class="button button-primary button-small mb-1 ml-1 whitespace-nowrap" type="button"
+                  @click="showSuperApproveModal = true">
             Super Approve
           </button>
         </div>
@@ -128,11 +135,11 @@
         </template>
         <template #body>
           <tr
-            class="cursor-pointer transition ease-in-out duration-200"
-            @click="openReportEdit(item)"
             v-for="(item, itemIndex) in reports.data"
             :key="itemIndex"
             :class="computedRowColour(itemIndex,item)"
+            class="cursor-pointer transition ease-in-out duration-200"
+            @click="openReportEdit(item)"
           >
             <TableTd>
               <div>
@@ -186,25 +193,25 @@
             </h3>
           </div>
           <div>
-            <button v-if="showSchoolInformation" @click="showSchoolInformation = false" type="button"
-                    class="text-gray-500 mr-4">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                   stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
+            <button v-if="showSchoolInformation" class="text-gray-500 mr-4" type="button"
+                    @click="showSchoolInformation = false">
+              <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                   xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 15l7-7 7 7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
               </svg>
             </button>
-            <button v-if="!showSchoolInformation" @click="showSchoolInformation = true" type="button"
-                    class="text-gray-500 mr-4">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                   stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            <button v-if="!showSchoolInformation" class="text-gray-500 mr-4" type="button"
+                    @click="showSchoolInformation = true">
+              <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                   xmlns="http://www.w3.org/2000/svg">
+                <path d="M19 9l-7 7-7-7" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
               </svg>
             </button>
           </div>
         </div>
       </div>
-      <div class="mt-5 border-t border-gray-200 transition transform translate duration-500 ease-in-out"
-           :class="!showSchoolInformation ? 'h-0':'h-100'"
+      <div :class="!showSchoolInformation ? 'h-0':'h-100'"
+           class="mt-5 border-t border-gray-200 transition transform translate duration-500 ease-in-out"
       >
         <DataDisplayContainer>
           <DataDisplayRow>
@@ -224,22 +231,23 @@
               Grades
             </template>
             <div class="flex flex-wrap">
-              <div class="relative ml-1 my-1" v-for="grade in school.grades.data">
+              <div v-for="grade in school.grades.data" class="relative ml-1 my-1">
                 <p class="bg-yellow-100 px-2 py-2 mx-2 mb-2 rounded-md">
                   <span>{{ grade.name }}</span>
                 </p>
                 <button v-if="$page.props.authUserRole === 'ADMIN' || $page.props.authUserRole === 'SUPER_ADMIN'"
-                        @click="deleteGradePreConfirm(grade)" class="absolute -top-1 right-0 text-red-500">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
-                       stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        class="absolute -top-1 right-0 text-red-500" @click="deleteGradePreConfirm(grade)">
+                  <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                       xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                          stroke-linecap="round" stroke-linejoin="round"
+                          stroke-width="2"/>
                   </svg>
                 </button>
               </div>
             </div>
             <button v-if="$page.props.authUserRole === 'ADMIN' || $page.props.authUserRole === 'SUPER_ADMIN'"
-                    @click="showAddGradeModal=true" class="button button-primary button-small">Add Grade
+                    class="button button-primary button-small" @click="showAddGradeModal=true">Add Grade
             </button>
           </DataDisplayRow>
           <DataDisplayRow>
@@ -250,17 +258,18 @@
               <div v-for="subject in school.subjects" class="relative bg-yellow-100 px-2 py-2 mx-2 mb-2 rounded-md">
                 <span class="uppercase">{{ subject.name }}</span>
                 <button v-if="$page.props.authUserRole === 'ADMIN' || $page.props.authUserRole === 'SUPER_ADMIN'"
-                        @click="deleteSubjectPreConfirm(subject)" class="absolute -top-2 -right-2 text-red-500">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
-                       stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        class="absolute -top-2 -right-2 text-red-500" @click="deleteSubjectPreConfirm(subject)">
+                  <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                       xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                          stroke-linecap="round" stroke-linejoin="round"
+                          stroke-width="2"/>
                   </svg>
                 </button>
               </div>
             </div>
             <button v-if="$page.props.authUserRole === 'ADMIN' || $page.props.authUserRole === 'SUPER_ADMIN'"
-                    @click="showAddSubjectModal=true" class="button button-primary button-small">Add Subject
+                    class="button button-primary button-small" @click="showAddSubjectModal=true">Add Subject
             </button>
           </DataDisplayRow>
           <DataDisplayRow>
@@ -271,27 +280,28 @@
               <div v-for="teacher in school.teachers" class="relative bg-yellow-100 px-2 py-2 mx-2 mb-2 rounded-md">
                 <span>{{ teacher.name }}</span>
                 <button v-if="$page.props.authUserRole === 'ADMIN' || $page.props.authUserRole === 'SUPER_ADMIN'"
-                        @click="removeTeacherPreConfirm(teacher)" class="absolute -top-2 -right-2 text-red-500">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
-                       stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        class="absolute -top-2 -right-2 text-red-500" @click="removeTeacherPreConfirm(teacher)">
+                  <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                       xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                          stroke-linecap="round" stroke-linejoin="round"
+                          stroke-width="2"/>
                   </svg>
                 </button>
               </div>
             </div>
             <button v-if="$page.props.authUserRole === 'ADMIN' || $page.props.authUserRole === 'SUPER_ADMIN'"
-                    @click="showAddTeacherModal=true" class="button button-primary button-small">Add Teacher
+                    class="button button-primary button-small" @click="showAddTeacherModal=true">Add Teacher
             </button>
           </DataDisplayRow>
         </DataDisplayContainer>
       </div>
     </Card>
-    <AddGradeModal v-model="showAddGradeModal" :school-id="school.id" />
-    <AddSubjectModal v-model="showAddSubjectModal" :school-id="school.id" />
-    <AddTeacherModal v-model="showAddTeacherModal" :school="school" />
-    <PrintModal v-model="showPrintModal" :print-list="printList" :school-id="school.id" />
-    <SuperApproveModal v-model="showSuperApproveModal" :school="school" :week="filterForm.filters.week" />
+    <AddGradeModal v-model="showAddGradeModal" :school-id="school.id"/>
+    <AddSubjectModal v-model="showAddSubjectModal" :school-id="school.id"/>
+    <AddTeacherModal v-model="showAddTeacherModal" :school="school"/>
+    <PrintModal v-model="showPrintModal" :print-list="printList" :school-id="school.id"/>
+    <SuperApproveModal v-model="showSuperApproveModal" :school="school" :week="filterForm.filters.week"/>
   </div>
 </template>
 
@@ -317,7 +327,8 @@ import {
   TrashIcon,
   CheckCircleIcon,
   ExclamationCircleIcon,
-  DocumentReportIcon
+  DocumentReportIcon,
+  CalendarIcon
 } from '@heroicons/vue/solid';
 import {useForm} from '@inertiajs/inertia-vue3';
 import Button from '@/Jetstream/Button';
@@ -340,6 +351,7 @@ export default {
     Card, PageHeading, Breadcrumbs, DataDisplayContainer, AddGradeModal,
     DataDisplayRow, PencilIcon, TrashIcon, ExternalLinkIcon, EyeIcon,
     ConfirmDialog, TableDisplayContainer, TableTh, TableTd, Link, DocumentReportIcon,
+    CalendarIcon,
     Pagination
   },
   layout: Layout,
